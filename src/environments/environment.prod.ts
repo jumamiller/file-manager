066f6b651b429dc668882ea63b754c9c5880df12
Miller Juma <miller.developer.william@gmail.com>
2021-09-12 12:34:31 +0300

    add auth

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   HomeURL:'https://development.kogas.ng/',
-  API_BASE_URL:'https://api-development.kogas.ng/api/'
+  API_BASE_URL:'https://api-development.kogas.ng/api/v1/'
 };
